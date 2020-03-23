ddf36e87afd0d73f76a1c52c94793479cf26de35
Pedro Minicz <pedrominicz@gmail.com>
2020-03-23 13:14:07 -0300

    Add post CSS.

--- vue/src/views/Post.vue
+++ vue/src/views/Post.vue
@@ -1,12 +1,16 @@
 <template>
-  <form @submit.prevent="submit">
-    <input type="text" placeholder="text" required v-model="text" autofocus>
-    <span v-for="category in categories" :key="category.name">
-      <input type="checkbox" :value="category.name" v-model="selected">
-      <label>{{ category.name }}</label>
-    </span>
-    <button type="submit">post</button>
-  </form>
+  <div>
+    <form @submit.prevent="submit">
+      <input type="text" placeholder="text" required v-model="text" autofocus>
+      <span v-for="category in categories" :key="category.name">
+        <input type="checkbox" :value="category.name" v-model="selected">
+        &nbsp;
+        <label>{{ category.name }}</label>
+        <br>
+      </span>
+      <button type="submit">post</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -34,4 +38,36 @@ export default {
 </script>
 
 <style scoped>
+button {
+  display: block;
+  margin: 5px auto;
+}
+
+div {
+  background: #f4f4f4;
+  margin: 50px auto;
+  max-width: 600px;
+  min-height: 250px;
+  position: relative;
+}
+
+form {
+  margin: 0 10px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: fill-available;
+}
+
+input {
+  margin: 5px 0;
+}
+
+input[type=text] {
+  width: 100%;
+}
+
+input[type=checkbox] {
+  border: solid gray;
+}
 </style>
